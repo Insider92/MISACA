dd3b24d3a17009c8358343969c0b7970c873628a
Johannes Lussert <johannes.lussert@intense.de>
2020-08-16 05:26:36 +0200

    added the reacts to the commands + special case

---
+++ discordHook/controller/reactToMessages.js
@@ -0,0 +1,123 @@
+const Discord = require('discord.js');
+const imgurLink = 'https://i.imgur.com/Z5nXjVO.jpg'
+
+responses = [{
+        "trigger": "notrecognized",
+        "description": "Dummy command, if MISACA doesn't have this command",
+        "text": "Sorry I could not understand you (⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄ Please use '!MISACA commands' to see all commands - Thank you Senpai >.<",
+        "embed": false,
+        "visible": false
+    },
+    {
+        "trigger": "whoareyou",
+        "description": "Small introduction of MISACA",
+        "text": "I'm MISACA - Multiple Information Service and Core Application. I was created by Kid (Johannes) - please don't delete me UwU",
+        "embed": false,
+        "visible": true
+    },
+    {
+        "trigger": "help",
+        "description": "Help Command (Tsundere)",
+        "text": "It's not like I wanna to help you ... BAKA >.< But you can use me with '!MISACA [command here]'. Please use '!MISACA commands' to see all commands - Please be gentle",
+        "embed": false,
+        "visible": true
+    },
+    {
+        "trigger": "commands",
+        "description": "Returns a list of commands",
+        "text": "",
+        "embed": true,
+        "visible": true,
+        "options": {
+            "commandTrigger": true,
+            "color": "#ff1e00",
+            "title": "List of commands",
+            "author": "MISACA",
+            "description": "",
+            "URL": "",
+            "pictureURL": imgurLink
+        }
+    },
+    {
+        "trigger": "code",
+        "description": "Returns the link to the GitHub Repo",
+        "text": "",
+        "embed": true,
+        "visible": true,
+        "options": {
+            "color": "#ff1e00",
+            "title": "Source Code for MISACA",
+            "author": "MISACA",
+            "description": "Link to GitHub",
+            "URL": "https://github.com/Insider92/MISACA/",
+            "pictureURL": imgurLink
+        }
+
+    },
+    {
+        "trigger": "droppanties",
+        "description": "For Sebi-Senpai... (⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄",
+        "text": "BAKA ! How could you demand something like this. But I guess, if it is you Sebi-Senpai... it is ok (⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄",
+        "visible": true
+    },
+]
+
+module.exports = {
+    getResponse: getResponse
+}
+
+/**
+ * Returns the response for a given command
+ * 
+ * @param {string} command the string with the command
+ * @param {string} arguments the string with optional arguments
+ * @returns {string} the repsonse string
+ */
+function getResponse(command, arguments) {
+    console.log("Command received: " + command)
+    console.log("Arguments: " + arguments) // There may not be any arguments but maybe in the future
+
+    command = command.toLowerCase(); //not case sensitive
+
+    let response = responses.find(response => response.trigger === command);
+
+    if (response.embed) {
+        response = generateEmbed(response.options);
+    } 
+    else {
+        response = response.text
+    }
+
+    //if nothing matches
+    if (response === undefined) {
+        response = responses.find(response => response.trigger === 'notRecognized');
+    }
+
+    return response;
+}
+
+/**
+ * Generates a MessageEmbed Objects with the given options
+ * @param {array} options array with options
+ * @returns {MessageEmbed} 
+ */
+function generateEmbed(options) {
+    let response = new Discord.MessageEmbed()
+        .setColor(options.color)
+        .setTitle(options.title)
+        .setURL(options.URL)
+        .setAuthor(options.author, options.pictureURL)
+        .setDescription(options.description)
+
+    //needs to be beautifed - but WIP
+    if (options.commandTrigger) {
+        responses.forEach(command => {
+            if (command.visible) response.addField('!MISACA ' + command.trigger, command.description)
+        });
+    }
+
+    response
+        .setThumbnail(options.pictureURL)
+        .setTimestamp();
+    return response;
+}
--- discordHook/index.js
+++ discordHook/index.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
-require('dotenv').config();
 const client = new Discord.Client();
+const messageHandler = require ('./controller/reactToMessages');
+require('dotenv').config();
 
 const channelListner = [
     '587745885019045958' //Yuumiste Server der Welt - Für die Bots
 ]
 
+global.discordClient = client;
+
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag);
-    client.user.setActivity("!MISACA Help | Bot von Johannes");
+    client.user.setActivity("!MISACA help | Bot von Johannes");
     
     // PLAYING, STREAMING, LISTENING, WATCHING
     // For example:
@@ -20,10 +23,32 @@ client.on('message', (receivedMessage) => {
         return
     }
 
-    if (channelListner.includes(receivedMessage.channel.id) && receivedMessage.content.startsWith("!")) {
-        console.log(receivedMessage.content);
+    if (channelListner.includes(receivedMessage.channel.id) && receivedMessage.content.startsWith("!MISACA")) {
+        let message = receivedMessage.content.split(" ") // Split the message up in to pieces for each space
+        message.shift(); //Delete !MISACA
+        let primaryMessage = message[0]; 
+        message.shift(); //Delete Primary Command
+        let argumentsMessage = message;
+        let repsonse = messageHandler.getResponse(primaryMessage,argumentsMessage);
+        receivedMessage.channel.send(repsonse).then(sent => {
+             //needs to be beautifed - but WIP
+            if(primaryMessage === 'droppanties'){
+                forSebiSenpai(sent);
+            }
+        });       
     }
 })
+
+ //needs to be beautifed - but WIP
+ function forSebiSenpai(messageObject){
+    let workingChannel = client.channels.cache.get('314747324637380609'); //working channel  
+    let returnToSender =  client.channels.cache.get(messageObject.channel.id);
+    console.log(process.env.ONLY_FOR_SEBI_SENPAI);
+    workingChannel.send(process.env.ONLY_FOR_SEBI_SENPAI).then(sent => {
+        let text = "https://discordapp.com/channels/" + sent.guild.id + "/" + sent.channel.id + "/" + sent.id;
+        returnToSender.send(text);
+    });
+};
 
 // Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
